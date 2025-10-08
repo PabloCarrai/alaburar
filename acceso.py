cf9aa6a3e192b1b4d607f3a044f85319dc4f6cea
PabloCarrai <pablohernanjuan@hotmail.com>
2025-10-08 15:24:17 -0300

    Funciona el registrar, hay que hacer lo del chequeo que la clave se ponga igual dos veces y que no se muestre la info

--- acceso.py
+++ acceso.py
@@ -1,9 +1,14 @@
 from tkinter import *
 from tkinter import ttk
+import gestordb
+from tkinter import messagebox as ms
 
 
 class acceso:
     def __init__(self):
+        self.usuarioSesion = ""
+        self.claveSesion = ""
+
         self.ventana = Tk()
         self.ventana.title("Bienvenido a AlAburaR")
         self.labelframe = LabelFrame(self.ventana, text="Credenciales")
@@ -21,10 +26,12 @@ class acceso:
 
         self.datoEntradaClaveUsuario = StringVar()
         self.entradaClaveUsuario = Entry(
-            self.labelframe, textvariable=self.datoEntradaClaveUsuario
+            self.labelframe, textvariable=self.datoEntradaClaveUsuario, show="*"
         )
         self.entradaClaveUsuario.grid(column=1, row=1, padx=10, pady=10)
-        self.botonIngresar = Button(self.labelframe, text="Ingresar")
+        self.botonIngresar = Button(
+            self.labelframe, text="Ingresar", command=self.ingresar
+        )
         self.botonIngresar.grid(column=0, row=2, padx=5, pady=5)
 
         self.botonRegistrar = Button(
@@ -33,6 +40,18 @@ class acceso:
         self.botonRegistrar.grid(column=1, row=2, padx=5, pady=5)
 
         self.ventana.mainloop()
+
+    def ingresar(self):
+        """ Aca hay que hacer bien el metodo, esto por ahora es prueba """
+        nombre = self.datoEntradaNombreUsuario.get()
+        self.usuarioSesion = nombre
+        clave = self.datoEntradaClaveUsuario.get()
+        self.claveSesion = clave
+        print(nombre, clave)
+        conexion = gestordb.gestordb()
+        resultado=conexion.consultarUsuario((nombre,))
+        for e in resultado:
+            print(e)
 
     def registrarUsuario(self):
         self.ventanaRegistrarUsuario = Toplevel()
@@ -60,26 +79,60 @@ class acceso:
             self.labelframeRegistroUsuario, text="Correo"
         )
         self.etiquetaRegistroUsuarioMail.grid(column=0, row=1, padx=10, pady=10)
-        self.datoEntradaRegistroUsuarioMail=StringVar()
-        self.entradaRegistroUsuarioMail=Entry(self.labelframeRegistroUsuario,textvariable=self.datoEntradaRegistroUsuarioMail)
-        self.entradaRegistroUsuarioMail.grid(column=1,row=1, padx=10, pady=10)
+        self.datoEntradaRegistroUsuarioMail = StringVar()
+        self.entradaRegistroUsuarioMail = Entry(
+            self.labelframeRegistroUsuario,
+            textvariable=self.datoEntradaRegistroUsuarioMail,
+        )
+        self.entradaRegistroUsuarioMail.grid(column=1, row=1, padx=10, pady=10)
 
-        self.etiquetaRegistroUsuarioClave=Label(self.labelframeRegistroUsuario,text="Clave")
-        self.etiquetaRegistroUsuarioClave.grid(column=0,row=2, padx=10, pady=10)
+        self.etiquetaRegistroUsuarioClave = Label(
+            self.labelframeRegistroUsuario, text="Clave"
+        )
+        self.etiquetaRegistroUsuarioClave.grid(column=0, row=2, padx=10, pady=10)
 
-        self.datoEntradaRegistroUsuarioClave=StringVar()
-        self.entradaRegistroUsuarioClave=Entry(self.labelframeRegistroUsuario,textvariable=self.datoEntradaRegistroUsuarioClave)
-        self.entradaRegistroUsuarioClave.grid(column=1,row=2, padx=10, pady=10)
+        self.datoEntradaRegistroUsuarioClave = StringVar()
+        self.entradaRegistroUsuarioClave = Entry(
+            self.labelframeRegistroUsuario,
+            textvariable=self.datoEntradaRegistroUsuarioClave,
+        )
+        self.entradaRegistroUsuarioClave.grid(column=1, row=2, padx=10, pady=10)
 
-        self.etiquetaRegistroUsuarioClaveReingreso=Label(self.labelframeRegistroUsuario,text="Clave")
-        self.etiquetaRegistroUsuarioClaveReingreso.grid(column=0,row=3, padx=10, pady=10)
+        self.etiquetaRegistroUsuarioClaveReingreso = Label(
+            self.labelframeRegistroUsuario, text="Clave"
+        )
+        self.etiquetaRegistroUsuarioClaveReingreso.grid(
+            column=0, row=3, padx=10, pady=10
+        )
 
-        self.datoEntradaRegistroUsuarioClave1=StringVar()
-        self.entradaRegistroUsuarioClave1=Entry(self.labelframeRegistroUsuario,textvariable=self.datoEntradaRegistroUsuarioClave1)
-        self.entradaRegistroUsuarioClave1.grid(column=1,row=3, padx=10, pady=10)
+        self.datoEntradaRegistroUsuarioClave1 = StringVar()
+        self.entradaRegistroUsuarioClave1 = Entry(
+            self.labelframeRegistroUsuario,
+            textvariable=self.datoEntradaRegistroUsuarioClave1,
+        )
+        self.entradaRegistroUsuarioClave1.grid(column=1, row=3, padx=10, pady=10)
 
-        self.botonRegistroUsuarioRegistrar=Button(self.labelframeRegistroUsuario,text="Registrar")
-        self.botonRegistroUsuarioRegistrar.grid(column=1,row=4, padx=10, pady=10)
+        self.botonRegistroUsuarioRegistrar = Button(
+            self.labelframeRegistroUsuario, text="Registrar",command=self.insertarUsuariodb
+        )
+        self.botonRegistroUsuarioRegistrar.grid(column=1, row=4, padx=10, pady=10)
+
+
+    def insertarUsuariodb(self):
+        nombre=self.datoEntradaRegistroUsuarioNombre.get()
+        correo=self.datoEntradaRegistroUsuarioMail.get()
+        clave=self.datoEntradaRegistroUsuarioClave.get()        
+        conexion = gestordb.gestordb()
+        datos=(nombre,correo,clave)
+        conexion.insertarUsuario(datos)
+        self.entradaRegistroUsuarioNombre.delete(0,END)
+        self.entradaRegistroUsuarioMail.delete(0,END)
+        self.entradaRegistroUsuarioClave.delete(0,END)
+        self.entradaRegistroUsuarioClave1.delete(0,END)
+        ms.showinfo("Registro de usuario Exitoso","Vamos carajo")        
+
+        
+
 
 
 acceso = acceso()
